a6e97abc0e2e082d29425105b9ecb93662b2f2ed
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-03-29 06:54:21 +0530

    Update 1.sql

--- 1.sql
+++ 1.sql
@@ -12,8 +12,8 @@ dept_name VARCHAR(50) NOT NULL,
  INSERT INTO Department VALUES(1,"HR","Kollam"),(2,"Sales","Kollam"),(3,"IT","Ernakulam");
  
 CREATE TABLE Employee( emp_no INT PRIMARY KEY,
- emp_name VARCHAR(50) NOT NULL, dob VARCHAR(10) NOT NULL,
- address VARCHAR(255) NOT NULL, doj VARCHAR(10) NOT NULL, 
+ emp_name VARCHAR(50) NOT NULL, dob DATE NOT NULL,
+ address VARCHAR(255) NOT NULL, doj DATE NOT NULL, 
  dept_no INT,
  FOREIGN KEY (dept_no) REFERENCES Department(dept_no),
  salary BIGINT NOT NULL);
