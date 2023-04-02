eb996f2e4d345994344958efe8f99042168eddbd
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-04-03 00:28:54 +0530

    Update Question1.sql

--- Question1.sql
+++ Question1.sql
@@ -9,19 +9,16 @@ CREATE TABLE  Department(dept_no INT PRIMARY KEY,
 dept_name VARCHAR(50) NOT NULL,
  location VARCHAR(50) NOT NULL);
 
- INSERT INTO Department VALUES(1,"HR","Kollam"),(2,"Sales","Kollam"),(3,"IT","Ernakulam");
- 
+
 CREATE TABLE Employee( emp_no INT PRIMARY KEY,
  emp_name VARCHAR(50) NOT NULL, dob DATE NOT NULL,
- address VARCHAR(255) NOT NULL, doj DATE NOT NULL, 
+ address VARCHAR(255) NOT NULL,
+ mobile BIGINT NOT NULL, doj DATE NOT NULL, 
  dept_no INT,
  FOREIGN KEY (dept_no) REFERENCES Department(dept_no),
  salary BIGINT NOT NULL);
  
  
- INSERT INTO Employee VALUES(110,"Ayash M","20/02/2000","Kunnikode , Punalur","22/03/2023",1,1000000),
- (111,"Nidal Basheer","15/10/2000","Vadakara, Kozhikode","25/03/2023",3,1200000),
- (112,"Devika Krishnan","10/01/2000","Ayathil , Kollam","28/03/2023",2,2000000);
  
  SHOW TABLES;
  
