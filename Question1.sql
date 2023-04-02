e0c777527d8b56f6adff38c62257099560e038e3
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-04-03 00:32:49 +0530

    Update Question1.sql

--- Question1.sql
+++ Question1.sql
@@ -13,7 +13,7 @@ dept_name VARCHAR(50) NOT NULL,
 CREATE TABLE Employee( emp_no INT PRIMARY KEY,
  emp_name VARCHAR(50) NOT NULL, dob DATE NOT NULL,
  address VARCHAR(255) NOT NULL,
- mobile BIGINT NOT NULL, doj DATE NOT NULL, 
+ mobile_no BIGINT NOT NULL, doj DATE NOT NULL, 
  dept_no INT,
  FOREIGN KEY (dept_no) REFERENCES Department(dept_no),
  salary BIGINT NOT NULL);
