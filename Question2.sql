8df7c1ab1f8f46f6b6c074779b700d2527d5045c
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-04-03 14:54:57 +0530

    Update Question2.sql

--- Question2.sql
+++ Question2.sql
@@ -3,7 +3,7 @@
 
  INSERT INTO Employee VALUES(110,"Ayash M","2000-02-20","Kunnikode , Punalur",1234567890,"2023-03-22",1,1000000,"HR Manager"),
  (111,"Nidal Basheer","2000-10-15","Vadakara, Kozhikode",1234512345,"2023-03-25",3,1200000,"IT Admin"),
- (112,"Devika Krishnan","2000-01-10","Ayathil , Kollam",0099887766,"2023-0-28",2,2000000,"Sales Representative"),
+ (112,"Devika Krishnan","2000-01-10","Ayathil , Kollam",0099887766,"2023-03-28",2,2000000,"Sales Representative"),
  (113,"Deva P Lal","2000-01-26","Pathanapuram , Kollam",1122334455,"2023-04-03",4,2000000,"Marketing Manager"),
  (114,"Athira Mohan","2002-03-10","Kottiyam , Kollam",0987654321,"2023-01-20",4,2000000,"Operations Assistant");
  
