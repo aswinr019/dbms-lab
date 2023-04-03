510440e3317946c8d56c121d889d45878d2bc819
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-04-03 15:44:05 +0530

    Update Question3.sql

--- Question3.sql
+++ Question3.sql
@@ -1,3 +1,5 @@
 SELECT emp_no , emp_name , salary  FROM Employee WHERE salary > ( SELECT AVG(salary) FROM Employee) ;
 
 SELECT emp_no , emp_name , salary  FROM Employee WHERE salary = ( SELECT MIN(salary) FROM Employee) ;
+
+SELECT  emp_name  FROM Employee WHERE designation = ( SELECT designation FROM Employee WHERE emp_name = "Arun") ;
