df95f30b750d01dff51866afc86c1650f135aa25
aswin r <119281218+aswinr019@users.noreply.github.com>
2023-04-09 21:18:03 +0530

    Update Question2.sql

--- Question2.sql
+++ Question2.sql
@@ -1,17 +1,16 @@
 
- INSERT INTO Department VALUES(1,"HR"),(2,"Sales"),(3,"IT"),(4,"Marketing"),(5,"Operations");
-
- INSERT INTO Employee VALUES(110,"Ayash M","2000-02-20","Kunnikode , Punalur",1234567890,"2023-03-22",1,1000000,"HR Manager"),
- (111,"Nidal Basheer","2000-10-15","Vadakara, Kozhikode",1234512345,"2023-03-25",3,1200000,"IT Admin"),
- (112,"Devika Krishnan","2000-01-10","Ayathil , Kollam",0099887766,"2023-03-28",2,2000000,"Sales Representative"),
- (113,"Deva P Lal","2000-01-26","Pathanapuram , Kollam",1122334455,"2023-04-03",4,2000000,"Marketing Manager"),
- (114,"Athira Mohan","2002-03-10","Kottiyam , Kollam",0987654321,"2023-01-20",4,2000000,"Operations Assistant");
+ INSERT INTO Department VALUES(1,"HR"),(2,"Sales"),(3,"IT"),(4,"Marketing"),(5,"Other");
+ 
+  INSERT INTO Employee VALUES
+  (110,'Ted J. Cloud', '1996-02-04', '4084 Lincoln Drive, Millerstown, PA 17062', '9829594246', '2022-04-23', '1', '50000', 'HR Recruiter'),
+    (111,'Larry S. Ackerman', '1997-04-20', '1885 Frederick Street, Midland, TX 79701', '4117136606', '2020-03-16', '3', '60000', 'IT Admin'),
+    (112,'John M. Patterson', '1995-09-12', '996 Simpson Square, Tonkawa, OK 74653', '8113899890', '2021-04-19', '2', '70000', 'Sales Head'),
+    (113,'Marlon L. Brennan', '1997-10-08', '4039 Griffin Street, Phoenix, AZ 85040', '6934074979', '2022-06-05', '4', '100000', 'Marketing Manager'),
+    (114,'Thomas C. Boston', '2000-11-03', '581 Michigan Avenue, New Castle, PA 16101', '6032868245', '2023-01-09', '5', '90000', 'Typist')
  
  
  SELECT * FROM Employee ;
  SELECT * FROM Department;
- 
---SELECT * FROM Employee as e INNER JOIN department as d ON e.dept_no = d.dept_no;
 
 SELECT emp_no, emp_name FROM Employee WHERE dept_no = 2;
 
@@ -25,7 +24,7 @@ SELECT dept_name, SUM(salary) FROM Employee e INNER JOIN department d ON e.dept_
 
 UPDATE Employee SET salary = 25000 WHERE designation = 'Typist';
 
-UPDATE Employee SET mobile_no = 1234567890 WHERE emp_name = 'Athira Mohan';
+UPDATE Employee SET mobile_no = 1234567890 WHERE emp_name = 'john';
 
 DELETE * FROM Employee WHERE salary = 7000;
 
